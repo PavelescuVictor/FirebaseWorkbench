aa4546ffc9c8d90d4a1d3bb4a6f303edd93a9dbb
Pavelescu Victor <pavelescu.victor97@gmail.com>
2021-03-08 00:24:16 +0200

    Small changes

--- public/app.js
+++ public/app.js
@@ -7,6 +7,33 @@
 
 document.addEventListener("DOMContentLoade", event => {
     const app = firebase.app();
+
+    // const dbFireStore = firebase.firestore();
+
+    // const authors = dbFireStore.collection('Authors');
+
+    // authors.get()
+    // .then(results => { 
+    //     console.log(results);
+    // })
+    // .catch(error => { 
+    //     console.error(error);
+    // })
+
+    // dbFireStore.collection("Authors").get().then((querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //         console.log(`${doc.id} => ${doc.data()}`);
+    //     });
+    // });
+
+    // const author = dbFireStore.collection('Authors').doc('Bla');
+    // author.get()
+    // .then(results => {
+    //     console.log(results.data());
+    // })
+    // .catch(error => {
+    //     console.error(error);
+    // })
 })
 
 /*
@@ -50,6 +77,16 @@ function loginWithRedirect(){
 }
 
 
+/*
+    @Description: Function for Requesting Data
+*/
+
+async function requestData(){
+    const snapshot = await firebase.firestore().collection('Authors').get()
+    console.log(snapshot.docs.map(doc => doc.data()));
+}
+
+
 // Adding event listner for the login with redirect button
 
 const loginButtonRedirect = document.querySelector(".login-button--redirect");
@@ -65,3 +102,12 @@ const loginButtonPopup = document.querySelector(".login-button--popup");
 loginButtonPopup.addEventListener("click", event => {
     loginWithPopup();
 })
+
+// Addong event listner for the request data button
+
+const requestDataButton = document.querySelector(".request-button");
+
+loginButtonPopup.addEventListener("click", event => {
+    requestData();
+})
+
